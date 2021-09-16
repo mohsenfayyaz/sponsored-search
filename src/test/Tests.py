dd3b514d7e8d5c71f5b78a0e14bb9b994b3943ea
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2021-09-16 13:03:45 +0430

    Add unittest

---
+++ src/test/Tests.py
@@ -0,0 +1,16 @@
+import unittest
+from src.SpanPooling import *
+
+
+class TestSpanPooling(unittest.TestCase):
+    def test_avg_pooling(self):
+        avg_pooling_module = get_pooling_module("avg")
+        span = torch.tensor([[[1, 1, 1], [2, 2, 2], [0, 0, 0]], [[1, 1, 1], [2, 2, 2], [3, 3, 3]]])
+        attention_mask = torch.tensor([[1, 1, 0], [1, 1, 1]])
+        pooled_representation = avg_pooling_module(span, attention_mask)
+        eq = torch.equal(pooled_representation, torch.tensor([[1.5, 1.5, 1.5], [2., 2., 2.]]))
+        self.assertTrue(eq)
+
+
+if __name__ == '__main__':
+    unittest.main()
